18ff9bed1404b717ad9a373a5d0c651856701ea7
Nicki Parker <NP204409@falmouth.ac.uk>
2018-05-01 19:21:00 +0100

    Adding and adapting slider function and assets

--- states/mission.js
+++ states/mission.js
@@ -4,8 +4,14 @@ Mission.prototype = {
 
   preload: function () {
     game.load.image('mission-bg', 'assets/images/mission-screen.png');
+    game.load.image('block1', 'assets/images/mission-slide.png');
+    game.load.image('block2', 'assets/images/rubbish-slide.png');
+    game.load.image('block3', 'assets/images/natural-slide.png');
+    game.load.image('block4', 'assets/images/large-object-slide.png');
+    game.load.image('block5', 'assets/images/goal-slide.png');
     this.optionCount = 1;
     this.creditCount = 0;
+    slider = new phaseSlider(game);
   },
 
   // addMission: function(task, author) {
@@ -61,6 +67,26 @@ Mission.prototype = {
 
     //adding background image to main menu
     game.missionBg = game.add.sprite(0, 0, 'mission-bg');
+
+    // add first slide
+    var block1 = game.add.image(0,0,"block1");
+    var block2 = game.add.image(0,0,"block2");
+    var block3 = game.add.image(0,0,"block3");
+    var block4 = game.add.image(0,0,"block4");
+    var block5 = game.add.image(0,0,"block5");
+
+    //adding slider function
+    slider.createSlider({
+      customSliderBG: false,
+      mode: "horizontal",
+      sliderBGAlpha: 0.01,
+      width: 332,
+      height: 340,
+      x: game.width / 2 - 332 / 2,
+      y: game.height / 2 - 340 / 1.1,
+      objects:[block1, block2, block3, block4, block5]
+    });
+
 
     this.stage.disableVisibilityChange = true;
     // if (gameOptions.playMusic) {
